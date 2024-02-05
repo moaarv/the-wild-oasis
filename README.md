2f80727d7dd9ffdb51eaa907768f0d70e6520b50
Moa Arvidsson <moa.arvidsson@redpill-linpro.com>
2024-02-05 15:15:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# The wild oasis
+
+Main project in Jonas Schmedtmann's Ultimate React Course.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ src/services/supabase.js
@@ -0,0 +1,17 @@
+/*import { createClient } from "@supabase/supabase-js";
+
+export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
+const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;*/
+
+
+import { createClient } from '@supabase/supabase-js';
+export const supabaseUrl = 'https://xzcrufzpxciuwooldxrt.supabase.co';
+const supabaseKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6Y3J1ZnpweGNpdXdvb2xkeHJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU2NjMyMjQsImV4cCI6MjAyMTIzOTIyNH0.mFjwxniYdZpffX0aQNSfPNTChIEHT9W6CH5KNbjscUE';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
